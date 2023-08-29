8d6a6b2e43fa74554f6e2e91756591ba20391818
Henrique Erinaldo <112641928+henriqueerds@users.noreply.github.com>
2023-08-29 14:33:08 +0000

    Create Projeto Java Basico

--- ContaBanco.java
+++ ContaBanco.java
@@ -1,1 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
 
+public class ContaBanco {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        ContaTerminal novaConta = new ContaTerminal();
+        
+        System.out.println("Digite seu nome: ");
+        novaConta.setNomeCliente(scanner.nextLine());
+
+        System.out.println("Digite o numero da sua Agência: ");
+        novaConta.setAgencia(scanner.nextLine());
+
+        System.out.println("Digite o numero da sua Conta: ");
+        novaConta.setNumero(scanner.nextInt());
+
+        System.out.println("Digite o seu Saldo: ");
+        novaConta.setSaldo(scanner.nextDouble());
+
+        System.out.println("Olá "+novaConta.getNomeCliente()+", obrigado por criar uma conta em nosso banco, sua agência é "+novaConta.getAgencia()+", conta "+novaConta.getNumero()+" e seu saldo "+novaConta.getSaldo()+" já está disponível para saque.");
+        
+    }
+}
